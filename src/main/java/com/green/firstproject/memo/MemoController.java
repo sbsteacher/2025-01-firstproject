dc2354b48a1831f9dd7a4917fe2926547c28bcb9
sbsteacher <id@gmail.com>
2025-07-15 11:33:49 +0900

    Get 메소드 추가

--- src/main/java/com/green/firstproject/memo/MemoController.java
+++ src/main/java/com/green/firstproject/memo/MemoController.java
@@ -1,15 +1,18 @@
 package com.green.firstproject.memo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/memo")
 public class MemoController {
 
     @GetMapping
     public String get() {
+        log.info("memo - get");
         return "memo";
     }
 }
